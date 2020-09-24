4ea64ab42c974b79ca93ecd9e0a9b9a50961a2dc
ray <827154659@qq.com>
2020-09-24 15:41:17 +0800

    "增加timeout重启函数循环"

--- my_downloader.py
+++ my_downloader.py
@@ -1,9 +1,22 @@
 import os
 import sys
 import warnings
+import re
 import requests
+import time
+
+
+url = "http://www.8080s.net/movie/32088"
 
 warnings.filterwarnings("ignore")
+# http请求建立连接的timeout设置
+connect_timeout = 15
+# 服务器返回数据包的timeout设置
+read_timeout = 120
+
+
+def formatFloat(num):
+    return '{:.2f}'.format(num)
 
 
 def down(url, dst):
@@ -27,18 +40,38 @@ def down(url, dst):
     # 核心部分，这个是请求下载时，从本地文件已经下载过的后面下载
     headers = {'Range': 'bytes=%d-' % temp_size}
     # 重新请求网址，加入新的请求头的
-    r = requests.get(url, stream=True, verify=False, headers=headers)
+    try:
+        r = requests.get(url, stream=True, verify=False, headers=headers, timeout=(connect_timeout, read_timeout))
 
-    # "ab"表示追加形式写入文件
-    with open(dst, "ab") as f:
-        for chunk in r.iter_content(chunk_size=1024):
-            if chunk:
-                temp_size += len(chunk)
-                f.write(chunk)
-                f.flush()
+        # 设置计时器监控下载速度
+        time1 = time.time()
+        pre_size = 0
 
-                ###这是下载实现进度显示####
-                done = int(50 * temp_size / total_size)
-                sys.stdout.write("\r[%s%s] %d%%" % ('█' * done, ' ' * (50 - done), 100 * temp_size / total_size))
-                sys.stdout.flush()
-    print()  # 避免上面\r 回车符
+        # "ab"表示追加形式写入文件
+        with open(dst, "ab") as f:
+            for chunk in r.iter_content(chunk_size=1024):
+                if chunk:
+                    temp_size += len(chunk)
+                    f.write(chunk)
+                    f.flush()
+
+                    piece_time = time.time() - time1
+                    # 下载速度计算
+                    speed = (temp_size - pre_size) / piece_time / 1024
+
+                    # 这是下载实现进度显示
+                    done = int(50 * temp_size / total_size)
+                    sys.stdout.write("\r[%s%s] %d%%, 下载速度：%s kb/s" % (
+                        '█' * done, ' ' * (50 - done), 100 * temp_size / total_size, formatFloat(speed)))
+                    sys.stdout.flush()
+        print()  # 避免上面\r 回车符
+    except requests.exceptions.Timeout as e:
+        print(e)
+        down(url, dst)
+
+
+if __name__ == '__main__':
+    res = requests.get(url)
+    content = res.text
+    a = re.findall(r"<a[^>]*\s+href=[\'\"]?([^\'\"]*)[\'\"]?[^>]*\s+>.*<\/", content)
+    down(a[0], './%s' % os.path.basename(a[0]))
